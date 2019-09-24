736dfdbd63e101fff272e9bf1e959bacf06fd82e
Dennis <denniskamau3@gmail.com>
2019-09-24 19:08:32 +0300

    add template and routing

--- pollster/polls/admin.py
+++ pollster/polls/admin.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from .models import Questions,Choice
 
+admin.site.site_header = "Pollster Admin"
+admin.site.site_title = "Pollster Admin Area"
+admin.site.index_title = "Welcome to Pollster admin Area"
+
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Questions,Choice
+
+def index(request):
+    return render(request,'polls/index.html')
