94e5bfdb1f52aa7d965c8a75455a716282ecfe49
Dennis <denniskamau3@gmail.com>
2019-09-24 19:34:37 +0300

    pass data to template

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -4,4 +4,6 @@ from django.shortcuts import render
 from .models import Questions,Choice
 
 def index(request):
-    return render(request,'polls/index.html')
+    latest_question_list = Questions.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list':latest_question_list}
+    return render(request,'polls/index.html',context)
