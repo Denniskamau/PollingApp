20791bb0d5ff121e530e0b8504eb169eb23de7b0
Dennis <denniskamau3@gmail.com>
2019-09-24 19:44:43 +0300

    result and details logic

--- pollster/polls/urls.py
+++ pollster/polls/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:question_id>',views.detail,name='detail'),
+    path('<int:question_id>/results/',views.results,name='results')
 ]
--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -7,3 +7,15 @@ def index(request):
     latest_question_list = Questions.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list':latest_question_list}
     return render(request,'polls/index.html',context)
+
+def detail(request,question_id):
+    try:
+        question = Questions.objects.get(pk=question_id)
+    except Questions.DoesNotExist:
+        raise Http404("Question DOes NOt Exist")
+
+    return render(request,'polls/results.html',{'question':question})
+
+def results(request,question_id):
+    question = get_object_or_404(Questions,pk=question_id)
+    return render(request,'polls/results.html',{'question':question})
