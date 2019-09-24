870c137cab4703bfad2d9967b2b59713d0de5cc1
Dennis <denniskamau3@gmail.com>
2019-09-24 18:24:02 +0300

    register the models in admin

--- pollster/polls/admin.py
+++ pollster/polls/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Questions,Choice
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields':['question_text']}),
+    ('Date Information', {'fields':['pub_date'],'classes':['collapse']}),]
+    inlines = [ChoiceInline]
+# admin.site.register(Questions)
+# admin.site.register(Choice)
+admin.site.register(Questions,QuestionAdmin)
--- pollster/polls/models.py
+++ pollster/polls/models.py
@@ -13,4 +13,4 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.choice_text 
+        return self.choice_text
